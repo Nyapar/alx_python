b912fa5877aeef4f503c1988d236d4cb4656aeab
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-07-23 16:33:53 +0000

    5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,6 @@
-for num in range(100):
-    print("{:02d}".format(num), end=", " if num != 99 else "\n")
-
+#!/usr/bin/python3
+for i in range(100):
+    if i < 99:
+        print("{:02}".format(i), end=", ")
+    else:
+        print("{:02}".format(i))
