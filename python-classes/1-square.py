673ae6919608370ccca44f879f6bfdcb8e6009c8
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-07-28 22:44:31 +0000

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
-
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
 
 try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+    square1 = Square(5)
+    print(square1.__size)
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
+    square2 = Square()     
+    print(square2.__size)  
 
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+    square3 = Square("hello")  
+    square4 = Square(-3)
+except TypeError as e:
+    print("TypeError:", e)
+except ValueError as e:
+    print("ValueError:", e)
+
