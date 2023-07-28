7b50819e7b85b799ddcc96dafce7b30930f07616
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-07-28 22:40:13 +0000

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
-"""A class Square that defines a square by: (based on 0-square.py)
-Private instance attribute: size
-Instantiation with size (no type/value verification)
-You are not allowed to import any module
-"""
+Square = __import__('1-square').Square
 
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
 
-class Square:
-    """Creates a square.
-    Private instance attributes: size
-    """
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
 
-    def __init__(self, size):
-        """Initializes size"""
-        self.__size = size
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
