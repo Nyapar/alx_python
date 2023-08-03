96bead733c1fcd8ac191c29cd02326417e3c47f0
Nyapar <malongmaker60@gmail.com>
2023-08-03 10:27:41 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,16 +4,6 @@
 
 
 class Square:
-     """Class Square that defines a square.
+    """Class Square that defines a square.
     """
-
-    def __init__(self, size):
-        self._size = size
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, size):
-        self._size = size
+    pass
