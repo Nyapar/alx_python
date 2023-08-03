1209741192f83435eefe2a5528a06e86eb4b25d6
Nyapar <malongmaker60@gmail.com>
2023-08-03 12:08:36 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,14 +9,21 @@ class Square:
     Private instance attributes: size
     """
 
-    def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+    def __init__(self, size):
         self.__size = size
 
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not init:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def area(self):
-        """Return square area."""
-        return self.__size ** 2
+        return self.size ** 2
