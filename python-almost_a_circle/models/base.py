d895981e14915ab1605a8b80aac0c26e21bc051d
Nyapar <malongmaker60@gmail.com>
2023-08-09 00:42:31 -0700

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
-class Base:
-    __nb_objects = 0
+from .base import Base
 
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+class Derived(Base):
+    def __init__(self, id=None, name=None):
+        super().__init__(id)
+        self.name = name
+
+instance1 = Derived()
+instance2 = Derived()
+print(instance1.id)
+print(instance2.id)
 
