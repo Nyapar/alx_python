295f542405d63db3ef731e2a63de23dd96f4aa1b
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-08-10 21:06:48 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import requests
 import sys
 
@@ -11,7 +10,7 @@ def get_request_id(url):
             return request_id
         else:
             return "X-Request-Id not found in the response header"
-    except requests.exceptions.RequestException as e:
+except requests.exceptions.RequestException as e:
         return f"An error occurred: {e}"
 
 if _name_ == "_main_":
