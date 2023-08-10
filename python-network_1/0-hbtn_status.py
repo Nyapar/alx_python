c28443901fb5cd32da2da2a6d2c411e335de7fe9
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-08-10 21:00:46 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,28 +2,18 @@
 import requests
  
 try:
-    # Making a get request to a valid URL
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    response = requests.get('https://alu-intranet.hbtn.io/status')
  
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Print the response content
         print(response.text)
     else:
-        # Print an error message if the request was not successful
-        print(f"Request failed with status code: {response.status_code}")
+        print(f"Request failed with status code:{response.status_code}")
+    response = requests.get('https://alu-intranet.hbtn.io/status')
  
-    # Making a get request to an incorrect URL
-    response = requests.get("https://alu-intranet.hbtn.io/status")
- 
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Print the response content
         print(response.text)
     else:
-        # Print an error message if the request was not successful
         print(f"Request failed with status code: {response.status_code}")
  
 except requests.exceptions.RequestException as e:
-    # Handle any exceptions that may occur during the request
     print(f"An error occurred: {e}")
