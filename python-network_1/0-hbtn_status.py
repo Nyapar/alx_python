504a6ba08271510db622362b49a121b3c41256f2
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-08-10 20:00:02 +0000

    0-hbtn_status.py

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print("Error:", response.status_code)
+
