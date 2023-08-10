9fd1b91ef7ceabb1784a61e3460253e62fb992b7
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-08-10 20:12:05 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,11 +3,7 @@ import requests
 url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    print("Body response:")
-    print("\t- type:", type(data))
-    print("\t- content:", data)
-else:
-    print("Error:", response.status_code)
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
 
