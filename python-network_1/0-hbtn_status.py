cce1a64439b44075a9a1e07ad0b48a6af79f95a6
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-08-10 20:23:29 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 import requests
 
 url = "https://alu-intranet.hbtn.io/status"
-response = requests.get(url)
-
-print("Body response:")
-print("\t- type:", type(response.text))
-print("\t- content:", response.text)
-
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception if the response status is not OK (200)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+except requests.exceptions.RequestException as e:
+    print("An error occurred:", e)
