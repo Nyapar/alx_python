531f8e53a9de6d792ea2455e7c0465e3d5802695
MALONGMAKER(Nyapar) <137778901+Nyapar@users.noreply.github.com>
2023-08-26 13:19:37 +0000

    model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+# The rest of your SQLAlchemy setup (creating engine and calling Base.metadata.create_all(engine)) should be done in a separate script.
+
