b8a0e8d252e3789cbabeceb887bbfaf00a506796
Malongmak <j.malong@alustudent.com>
2023-09-25 00:15:16 +0200

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,21 @@
-import MySQLdb
+#!/usr/bin/python3
+"""
+Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
+import MySQLdb
 
-def search_states(username, password, database_name, state_name):
-    # Connect to the MySQL server
-    try:
-        connection = MySQLdb.connect(user=username, passwd=password, db=database_name, host='localhost', port=3306)
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # Create a cursor object to interact with the database
-    cursor = connection.cursor()
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    # Prepare the parameterized SQL query and execute it
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all the rows and display the results
-    states = cursor.fetchall()
     for state in states:
         print(state)
-
-    # Close the cursor and the database connection
-    cursor.close()
-    connection.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database_name> <state_name>")
-        sys.exit(1)
-
-    username, password, database_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    search_states(username, password, database_name, state_name)
-
