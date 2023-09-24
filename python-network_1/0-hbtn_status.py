ef3ca34d055ed215085f19c91450af3099b609ef
Malongmak <j.malong@alustudent.com>
2023-09-24 23:32:47 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,11 @@
-# import requests module
+#!/usr/bin/python3
 import requests
- 
-try:
-    response = requests.get('https://alu-intranet.hbtn.io/status')
- 
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        print(f"Request failed with status code:{response.status_code}")
-    response = requests.get('https://alu-intranet.hbtn.io/status')
- 
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        print(f"Request failed with status code: {response.status_code}")
- 
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+
