a2a6e1bf1cdae519593dd430d5cc7ca33e7da69b
Malongmak <j.malong@alustudent.com>
2023-09-24 23:59:03 +0200

    4-error_code.py

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,29 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and displays the body of the response."""
 import requests
 import sys
 
-def get_github_user_id(username, pat):
-    try:
-        url = "https://api.github.com/user"
-        response = requests.get(url, auth=(username, pat))
-
-        if response.status_code == 200:
-            user_info = response.json()
-            user_id = user_info.get('id')
-            if user_id:
-                print("GitHub User ID:", user_id)
-            else:
-                print("User ID not found in the response")
-        else:
-            print("Error:", response.text)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-
-if _name_ == "_main_":
-    if len(sys.argv) != 3:
-        print("Usage: ./github_user_id.py <Nyapar> <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>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    pat = sys.argv[2]
-
-    get_github_user_id(Nyapar, pat)
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print("{}".format(response.text))
