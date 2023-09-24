ece6514e9e6f424ae3ede98d893c96893c50ce59
Malongmak <j.malong@alustudent.com>
2023-09-25 00:04:07 +0200

    6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,19 +1,20 @@
+#!/usr/bin/python3
+""" Python script that uses the GitHub API to display your id"""
 import requests
+import requests.auth
 import sys
 
-def main():
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-
-    auth = (username, password)
-    response = requests.get('https://api.github.com/user', auth=auth)
-
-    if response.status_code == 200:
-        user_data = response.json()
-        print("Your GitHub user id:", user_data['id'])
-    else:
-        print("Error:", response.status_code)
-
-if __name__ == "__main__":
-    main()
-
+    response = requests.get(
+        url="https://api.github.com/user",
+        auth=(requests.auth.HTTPBasicAuth(
+            username,
+            password
+        )))
+    try:
+        json_response = response.json()
+        print("{}".format(json_response["id"]))
+    except:
+        print(None)
