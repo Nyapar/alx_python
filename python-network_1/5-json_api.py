acf8624212198fb199bc34b10c51667c988c2bfe
Malongmak <j.malong@alustudent.com>
2023-09-25 00:01:22 +0200

    5-json_api.py

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,25 +1,26 @@
+#!/usr/bin/python3
+"""Search API"""
 import requests
 import sys
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <letter>")
-        return
-
-    letter = sys.argv[1]
-    payload = {'q': letter}
-
-    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-
+if __name__ == '__main__':
     try:
-        json_data = response.json()
-        if json_data:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
-        else:
-            print("No result")
-    except ValueError:
+        params = sys.argv[1]
+    except IndexError:
+        params = ""
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user",
+        data={"q": params}
+    )
+    try:
+        json_response = response.json()
+        if response.headers.get("Content-Type") == 'application/json':
+            if len(json_response) > 0:
+                print("[{}] {}".format(
+                    json_response["id"],
+                    json_response["name"])
+                )
+            else:
+                print("No result")
+    except:
         print("Not a valid JSON")
-
-if __name__ == "__main__":
-    main()
-
